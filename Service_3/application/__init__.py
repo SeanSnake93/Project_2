9e8a0a9ae235dfff2b2b33054a10a54cd1519b16
SeanSnake93 <seansnake93@hotmail.co.uk>
2020-06-05 14:54:57 +0000

    uploaded 05-06-2020 @ 15:54

--- Service_3/application/__init__.py
+++ Service_3/application/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import requests
-from os import getenv
-from flask_bycrypt import Bycrypt
+import os
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SQLACHEMY_DATABASE_URI'] = str(getenv( 'PROJECT_URI' ))
+app.config['SQLACHEMY_DATABASE_URI'] = str(os.getenv( 'PROJECT_URI' ))
 app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = str(getenv( 'MY_SECRET_KEY' ))
+app.config['SECRET_KEY'] = str(os.getenv( 'MY_SECRET_KEY' ))
 db = SQLAlchemy(app)
-bycrypt = Bycrypt(app)
+bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
