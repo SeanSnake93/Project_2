5799de37d4669d1105d8025c90045d16cad79553
seansnake93 <seansnake93@hotmail.co.uk>
2020-06-10 17:25:31 +0000

    uploaded 10-06-2020 @ 18:25

--- Service_2/application/routes.py
+++ Service_2/application/routes.py
@@ -1,12 +1,13 @@
 from application import app
-from flask import request, Responce
+from flask import request, Response, jsonify
 from random import randrange
 import requests
 
 @app.route('/generate', methods=['GET', 'POST'])
 def generate():
-    genre = requests.get('http://service_3:5002/randomgenre') # Call Service 3 to to return a Genre to filter a Movie form Service 4.
+    genre = requests.get("http://service_3:5002/randomgenre").text # Call Service 3 to to return a Genre to filter a Movie form Service 4.
     print("Service 3 code:", genre) # Print/Display responce code on Terminal.#
-    responce = requests.get('http://service_4:5003/movie', data=genre.text) # With the Genre defined, filter a Movie in Service 4 related to this Genre.
-    movie = responce.json()
-    return "We have selected " + movie.title + " from our " + genre.text + " Collection." # The Responce being sent back to be displayed on site.
+    movie = requests.post("http://service_4:5003/movie", genre).text # With the Genre defined, filter a Movie in Service 4 related to this Genre.
+    # movie = response.json()
+    # add generated list table
+    return Response("We have selected " + movie + " from our " + genre + " Collection.") # The Responce being sent back to be displayed on site.
--- Service_3/application/routes.py
+++ Service_3/application/routes.py
@@ -1,8 +1,11 @@
-from application import app
-from flask import request, Responce
-from random import randrange
+from application import app, db
+from application.models import Genres
+from flask import request, Response
+from random import randint
 
-@app.route('/randomgenre', methods=['GET'])
+@app.route('/randomgenre', methods=['GET', 'POST'])
 def randomgenre():
-    data = Genre.query.all().genre
-    return Responce(data[randrange(len(data))], mimetype='text/plain')
+    length = Genres.query.count() #Count number of records in genre
+    ran = randint(1, length) # Generate random num between 1 and length
+    genreRow = Genres.query.filter_by(id = ran).first() # Find genreID and genre where genre ID = random number
+    return genreRow.genre # Return random genre
